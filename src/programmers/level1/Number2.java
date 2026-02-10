1be0534fe45919f22f10ac7950a7ded8ed8c3710
hjk2132 <xiest@naver.com>
2026-02-10 11:59:37 +0900

    solve : Programmers 120804

---
+++ src/programmers/level1/Number2.java
@@ -0,0 +1,11 @@
+package programmers.level1;
+
+// url: https://school.programmers.co.kr/learn/courses/30/lessons/120804
+public class Number2 {
+    public int solution(int num1, int num2) {
+        int result = num1 * num2;
+        System.out.println(result);
+
+        return result;
+    }
+}
