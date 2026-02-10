4653a63d47f768b72068746c6b79920b74d34e4c
hjk2132 <xiest@naver.com>
2026-02-10 11:55:08 +0900

    solve : Programmers 120803

---
+++ src/programmers/level1/Number1.java
@@ -0,0 +1,11 @@
+package programmers.level1;
+
+// url: https://school.programmers.co.kr/learn/courses/30/lessons/120803
+public class Number1 {
+    public int solution(int num1, int num2) {
+        int result = num1 - num2;
+        System.out.println(result);
+
+        return result;
+    }
+}
