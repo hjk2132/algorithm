67e5471bcacfacf227ef870af67aa462d20dd4b6
hjk2132 <xiest@naver.com>
2026-02-10 12:06:40 +0900

    solve : Programmers 120805

---
+++ src/programmers/level1/Number3.java
@@ -0,0 +1,12 @@
+package programmers.level1;
+
+// url : https://school.programmers.co.kr/learn/courses/30/lessons/120805
+public class Number3 {
+    public int solution(int num1, int num2) {
+        int result = num1 / num2;
+
+        System.out.println(result);
+
+        return result;
+    }
+}
