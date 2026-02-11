b9edf07708defbf08bf6e98f39ac9d5072075b08
hjk2132 <xiest@naver.com>
2026-02-11 10:20:45 +0900

    solve : Programmers 120802

---
+++ src/programmers/level1/Number6.java
@@ -0,0 +1,10 @@
+package programmers.level1;
+
+// url : https://school.programmers.co.kr/learn/courses/30/lessons/120802
+public class Number6 {
+    public int solution(int num1, int num2) {
+        int result = num1 + num2;
+
+        return result;
+    }
+}
