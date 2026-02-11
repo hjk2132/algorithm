b5384800be6db718539cf4b26ff82e866bf9445e
hjk2132 <xiest@naver.com>
2026-02-11 10:15:18 +0900

    fix : add url about Programmers 120807

--- src/programmers/level1/Number5.java
+++ src/programmers/level1/Number5.java
@@ -1,5 +1,6 @@
 package programmers.level1;
 
+// url : https://school.programmers.co.kr/learn/courses/30/lessons/120807?language=java
 public class Number5 {
     public int solution(int num1, int num2) {
         if (num1 == num2) {
