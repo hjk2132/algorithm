ea4be7f481379dc9a09358c3a95c2a03d9cf13e8
홍준기 <170405684+hjk2132@users.noreply.github.com>
2026-02-14 22:29:39 +0900

    solve : Programmers 12925

---
+++ src/programmers/level1/Number18.java
@@ -0,0 +1,12 @@
+package programmers.level1;
+
+// url : https://school.programmers.co.kr/learn/courses/30/lessons/12925
+public class Number18 {
+    class Solution {
+        public int solution(String s) {
+            int result = Integer.parseInt(s);
+            System.out.println(s);
+            return result;
+        }
+    }
+}
