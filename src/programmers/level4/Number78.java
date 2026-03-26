a645fda3c0f633945b40d9b59e837ab68811a4e2
hjk2132 <xiest@naver.com>
2026-03-26 23:27:00 +0900

    solve : Programmers 12945

---
+++ src/programmers/level4/Number78.java
@@ -0,0 +1,17 @@
+package programmers.level4;
+
+// url : https://school.programmers.co.kr/learn/courses/30/lessons/12945
+public class Number78 {
+    public int solution(int n) {
+        int[] fibonacci = new int[n + 1];
+
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2]) % 1234567;
+        }
+
+        return fibonacci[n];
+    }
+}
