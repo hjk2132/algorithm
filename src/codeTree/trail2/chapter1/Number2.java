6398e19d78ac41a7a20d869dca74035553f30ad0
hjk2132 <xiest@naver.com>
2026-06-01 17:03:58 +0900

    solve : CodeTree Trail2 Chapter1 Number2

---
+++ src/codeTree/trail2/chapter1/Number2.java
@@ -0,0 +1,20 @@
+package codeTree.trail2.chapter1;
+
+import java.util.Scanner;
+
+// url : https://www.codetree.ai/ko/trails/complete/curated-cards/intro-repeated-output/description
+public class Number2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+
+        printN(input);
+        sc.close();
+    }
+
+    public static void printN(int n) {
+        for (int i = 0; i < n; ++i) {
+            System.out.println("12345^&*()_");
+        }
+    }
+}
