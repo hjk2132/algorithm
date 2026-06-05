7f55e48e0cb19f4fef65cd8b3dc562b607661249
hjk2132 <xiest@naver.com>
2026-06-05 21:47:27 +0900

    solve : CodeTree Trail2 Chapter1 Lesson2 Number5

---
+++ src/codeTree/trail2/Number5.java
@@ -0,0 +1,42 @@
+package codeTree.trail2;
+
+import java.util.Scanner;
+
+// url : https://www.codetree.ai/ko/trails/complete/curated-cards/intro-decimal-decisions-using-functions/description
+public class Number5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = a; i <= b; ++i) {
+            sum = sum + isPrime(i);
+        }
+
+        System.out.println(sum);
+    }
+
+    public static int isPrime(int input) {
+        if (input < 2) {
+            return 0;
+        } else {
+            boolean isAdd = false;
+
+            for (int i = 2; i < input; ++i) {
+                if (input % i == 0) {
+                    isAdd = true;
+                    break;
+                }
+            }
+
+            if (isAdd == false) {
+                return input;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
