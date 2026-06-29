fcbc6d8b1600a403185ccaab0ea40fe8df00be71
hjk2132 <xiest@naver.com>
2026-06-29 11:08:40 +0900

    solve : CodeTree Trail2 Chapter2 Lesson1 Number1

---
+++ src/codeTree/trail2/chapter2/lesson1/Number1.java
@@ -0,0 +1,15 @@
+package codeTree.trail2.chapter2.lesson1;
+
+import java.util.Scanner;
+
+// url : https://www.codetree.ai/ko/trails/complete/curated-cards/intro-repeated-output-2/description
+public class Number1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+
+        for (int i = 0; i < input; ++i) {
+            System.out.println("HelloWorld");
+        }
+    }
+}
